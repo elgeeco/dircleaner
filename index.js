ed54cdc1883ae87e77861b5fdd07cc74bd944c6f
elgeeco <ernie.punk@gmx.de>
2014-03-27 21:04:31 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+
+function _deleteFolderRecursive(dirpath) {
+
+    if( fs.existsSync(dirpath) ) {
+       
+		fs.readdirSync(dirpath).forEach(function(file,index){
+		
+            var curPath = dirpath + path.sep + file;
+			
+            if(fs.lstatSync(curPath).isDirectory()) { 
+                _deleteFolderRecursive(curPath);
+            } else { 
+                fs.unlinkSync(curPath);
+            }
+			
+        });
+        fs.rmdirSync(dirpath);
+    }
+	
+};
+
+
+exports.clean = function(dirpath){
+    
+	_deleteFolderRecursive(dirpath);
+
+    if(!fs.existsSync( dirpath )){
+        fs.mkdirSync(dirpath,  function(err){
+            if(err) return console.log(err.message);
+        });
+    }
+}
